9679ea54ea47b95648ff14b309f21ba92ece1e1e
playerr0r <clirr@yahoo.com>
2023-04-14 13:06:48 +0300

    add UCS

--- lab1.py
+++ lab1.py
@@ -6,8 +6,7 @@
 # Размер доски
 N = 8
 # Координаты клеток, вырезанных из доски
-ex = [[4, 1], [6, 7], [2, 1], [1, 0], [6, 4], [0, 3], [5, 4], [4, 5], [0, 4], [6, 6], [4, 6], [3, 4], [4, 0], [7, 4], [7, 5], [2, 3], [1, 6], [3, 7], [4, 2], [5, 6], [5, 3], [5, 1], [1, 5], [7, 0], [0, 5], [1, 4], [5, 0], [5, 2], [6, 1], [4, 3], [7, 4]]
-
+ex = [[1,2], [0,1], [3,2], [2,2], [4,2], [4,1], [4, 0], [3,0], [5,0], [5,1], [6,1], [7,0], [7, 3], [5,3], [4,3], [0, 4], [1, 4], [2, 4], [3, 4], [5, 4], [6, 4], [6, 5], [6, 6], [7, 6], [0, 7], [2, 7], [3, 7], [4,7], [5,7], [1,6], [2,6], [3,6], [4,6], [0,5], [1, 5], [2,5], [3,5], [4,5]]
 # Координаты начальной и конечной клеток
 sx, sy = 0, 0
 fx, fy = 5, 5
@@ -32,17 +31,18 @@ def isFinal(x, y):
     return x == fx and y == fy
 
 
-#TODO: глубину нужно ограничивать
-
 # поиск пути с ограничением помощью поиска в глубину
 def findPathDepthLimited():
-    print("Depth Limited")
+    print("Depth limited")
+    limit = 5
     # Список клеток, доступных для хода
     queue = [[sx, sy]]
     # Список клеток, по которым уже ходили
     visited = []
     # Список клеток, из которых можно попасть в текущую
     parents = [[None, None] for i in range(N * N)]
+    # Список расстояний от начальной клетки до текущей
+    g = [0 for i in range(N * N)]
     # Пока есть доступные клетки
     while len(queue) > 0:
         # Берем первую клетку из очереди
@@ -55,6 +55,7 @@ def findPathDepthLimited():
                 x, y = parents[y * N + x]
             path.append([sx, sy])
             path.reverse()
+            print(path)
             return path
         # Добавляем клетку в список посещенных
         visited.append([x, y])
@@ -64,9 +65,14 @@ def findPathDepthLimited():
             if isAccessible(x + dx, y + dy) and [x + dx, y + dy] not in visited:
                 # Добавляем клетку в список доступных
                 queue.append([x + dx, y + dy])
-                # Запоминаем, откуда можно попасть в эту клетку
-                parents[(y + dy) * N + (x + dx)] = [x, y]
-        # print(visited)
+                # Запоминаем клетку, из которой можно попасть в текущую
+                parents[(y + dy) * N + x + dx] = [x, y]
+                # Запоминаем расстояние от начальной клетки до текущей
+                g[y * N + x] = g[y * N + x] + 1
+                # Если достигли лимита, то удаляем клетку из списка доступных
+                if g[y * N + x] == limit:
+                    queue.remove([x + dx, y + dy])
+    # Если путь не найден, то возвращаем None
     return None
 
 
@@ -116,6 +122,49 @@ def findPathAStar():
                 f[y * N + x] = g[y * N + x] + h[y * N + x]
         # Сортируем список доступных клеток по расстоянию от начальной до конечной
         queue.sort(key=lambda i: f[i[1] * N + i[0]])
+        # print(visited)
+    return None
+
+# поиск пути с помощью UCS(Uniform Cost Search)
+def findPathUCS():
+    print("Uniform Cost Search")
+    # Список клеток, доступных для хода
+    queue = [[sx, sy]]
+    # Список клеток, по которым уже ходили
+    visited = []
+    # Список клеток, из которых можно попасть в текущую
+    parents = [[None, None] for i in range(N * N)]
+    # Список расстояний от начальной клетки до текущей
+    g = [0 for i in range(N * N)]
+    # Цены ходов
+    costs = [1 for i in range(N * N)]
+    # Пока есть доступные клетки
+    while len(queue) > 0:
+        # Берем первую клетку из очереди
+        x, y = queue.pop(0)
+        # Если клетка является конечной, то возвращаем путь
+        if isFinal(x, y):
+            path = []
+            while x != sx or y != sy:
+                path.append([x, y])
+                x, y = parents[y * N + x]
+            path.append([sx, sy])
+            path.reverse()
+            return path
+        # Добавляем клетку в список посещенных
+        visited.append([x, y])
+        # Для каждого возможного хода
+        for dx, dy in [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]:
+            # Если клетка доступна и не посещена
+            if isAccessible(x + dx, y + dy) and [x + dx, y + dy] not in visited:
+                # Добавляем клетку в список доступных
+                queue.append([x + dx, y + dy])
+                # Запоминаем клетку, из которой можно попасть в текущую
+                parents[(y + dy) * N + x + dx] = [x, y]
+                # Запоминаем расстояние от начальной клетки до текущей
+                g[y * N + x] = g[y * N + x] + costs[y * N + x]
+        # Сортируем список доступных клеток по расстоянию от начальной до текущей
+        queue.sort(key=lambda i: g[i[1] * N + i[0]])
         # print(visited)
     return None
 
@@ -173,7 +222,7 @@ def printPath(path):
                 elif [j, i] == [sx, sy]:
                     print('S', end=' ')
                 # Если клетка входит в путь
-                if [j, i] in path:
+                elif [j, i] in path:
                     print('X', end=' ')
                 else:
                     print('.', end=' ')
@@ -184,3 +233,4 @@ def printPath(path):
 printPath(path = findPathDepthLimited())
 printPath(path = findPathBFS())
 printPath(path = findPathAStar())
+printPath(path = findPathUCS())
